73b39b70656b6cfb7105d17047cb68b6889ab237
Luiz Guilherme <37851595+Luiizrst@users.noreply.github.com>
2022-07-21 18:41:34 +0000

    Atividade Lab 10

---
+++ build/CMakeFiles/bitStream.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/workspaces/dcc012-lab-bitstream/bitstream.cpp" "/workspaces/dcc012-lab-bitstream/build/CMakeFiles/bitStream.dir/bitstream.cpp.o"
+  "/workspaces/dcc012-lab-bitstream/main.cpp" "/workspaces/dcc012-lab-bitstream/build/CMakeFiles/bitStream.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/bitStream.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bitStream.dir/bitstream.cpp.o"
+  "CMakeFiles/bitStream.dir/main.cpp.o"
+  "bitStream"
+  "bitStream.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bitStream.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
